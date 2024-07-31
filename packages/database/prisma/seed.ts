4168ab1b5e420a0300c11fe1e0d7a27c42d35390
Shahbaz <shahbazathwal2107@gmail.com>
2024-07-31 12:49:22 +0530

    seed error fix

--- packages/database/prisma/seed.ts
+++ packages/database/prisma/seed.ts
@@ -54,11 +54,11 @@ async function main() {
     data: {
       courseCode: "COMP1113",
       courseName: "Computer Programming 1",
-      notionDocId: "COMP1113-Computer-Programming-1-7e847d61e6244af48205e9bbd91d2166",
+      docId: "COMP1113-Computer-Programming-1-7e847d61e6244af48205e9bbd91d2166",
       department: {
         connect: { id: compSciDept.id },
       },
-      professor: {
+      professors: {
         connect: [{ id: darcy.id }, { id: greg.id }],
       },
     },
@@ -68,11 +68,11 @@ async function main() {
     data: {
       courseCode: "COMP2213",
       courseName: "Data Structures and Algorithms",
-      notionDocId: "COMP2213-Data-Structures-and-Algorithms-24bc04260fc24bac9606453d1ed44db8",
+      docId: "COMP2213-Data-Structures-and-Algorithms-24bc04260fc24bac9606453d1ed44db8",
       department: {
         connect: { id: compSciDept.id },
       },
-      professor: {
+      professors: {
         connect: [{ id: darcy.id }, { id: greg.id }],
       },
     },
@@ -82,11 +82,11 @@ async function main() {
     data: {
       courseCode: "COMP1123",
       courseName: "Computer Programming 2",
-      notionDocId: "COMP1123-Computer-Programming-2-3b8cd659b7d34f1ea7ea33be53b4a1e5",
+      docId: "COMP1123-Computer-Programming-2-3b8cd659b7d34f1ea7ea33be53b4a1e5",
       department: {
         connect: { id: compSciDept.id },
       },
-      professor: {
+      professors: {
         connect: [{ id: greg.id }],
       },
     },
@@ -96,11 +96,11 @@ async function main() {
     data: {
       courseCode: "MATH1013",
       courseName: "Introductory Calculus 1",
-      notionDocId: "MATH1013-Introductory-Calculus-1-5d13f3f75cfb4de4bfebf9b44db08e1e",
+      docId: "MATH1013-Introductory-Calculus-1-5d13f3f75cfb4de4bfebf9b44db08e1e",
       department: {
         connect: { id: mathDept.id },
       },
-      professor: {
+      professors: {
         connect: [{ id: ian.id }, {id: caro.id}],
       },
     },
@@ -110,11 +110,11 @@ async function main() {
     data: {
       courseCode: "MATH1253",
       courseName: "Statistics 1",
-      notionDocId: "MATH1253-Statistics-1-6e04cf0829f5433d82c24b603e9a7332",
+      docId: "MATH1253-Statistics-1-6e04cf0829f5433d82c24b603e9a7332",
       department: {
         connect: { id: mathDept.id },
       },
-      professor: {
+      professors: {
         connect: { id: ian.id },
       },
     },
