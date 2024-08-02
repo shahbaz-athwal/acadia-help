5246e94dd8fff282b546906342bb6dbd9ef4bc5d
Mehir Kumar <mehirk28@gmail.com>
2024-08-02 10:38:32 +0400

    added more courses to the list

--- apps/web/src/lib/coursesData.ts
+++ apps/web/src/lib/coursesData.ts
@@ -18,6 +18,33 @@ export const courses = [
       description: null,
       docId: 'COMP2213-Data-Structures-and-Algorithms-24bc04260fc24bac9606453d1ed44db8',
       isDocReady: false
+    },
+    {
+      id: '5ecca1d6-357f-4eff-b3fb-93ac3eeb3745',
+      courseCode: 'COMP1123',
+      courseName: 'Computer Programming 2',
+      departmentId: 1,
+      description: null,
+      docId: 'COMP1123-Computer-Programming-2-3b8cd659b7d34f1ea7ea33be53b4a1e5',
+      isDocReady: false
+    },
+    {
+      id: '8ed30401-75ba-4774-a310-584c1a6e73fd',
+      courseCode: 'MATH1013',
+      courseName: 'Introductory Calculus 1',
+      departmentId: 2,
+      description: null,
+      docId: 'MATH1013-Introductory-Calculus-1-5d13f3f75cfb4de4bfebf9b44db08e1e',
+      isDocReady: false
+    },
+    {
+      id: '74c79490-c5c1-44b2-8e6e-9593ead85562',
+      courseCode: 'MATH1253',
+      courseName: 'Statistics 1',
+      departmentId: 2,
+      description: null,
+      docId: 'MATH1253-Statistics-1-6e04cf0829f5433d82c24b603e9a7332',      
+      isDocReady: false
     }
   ];
   
