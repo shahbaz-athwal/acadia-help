82fe6766e6d5b64cf006677191b415524fb75fe0
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-03 14:52:48 +0530

    navbar layouted

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Navbar } from "../components/Navbar";
 import CourseList from '../components/CourseList';
 import CoursesHeader from '@/components/CoursesHeader';
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <div className='pt-10'>
         <CoursesHeader />
         <div className='container mx-auto px-4 py-8'>
