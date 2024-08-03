49aa314cdf9b7a0c85763d381350dcc5ae24fd4f
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-03 13:56:04 +0530

    add course ui improved

--- apps/web/src/app/admin/AddCourse.tsx
+++ apps/web/src/app/admin/AddCourse.tsx
@@ -1,4 +1,16 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { addCourse } from "@/lib/dbQueries";
 import { useState } from "react";
 
@@ -6,39 +18,81 @@ const AddCourse = () => {
   const [courseCode, setCourseCode] = useState("");
   const [courseName, setCourseName] = useState("");
   const [description, setDescription] = useState("");
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleSubmit = async () => {
-    addCourse({
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    await addCourse({
       courseCode,
       courseName,
-      department: { connect: { prefix: courseCode.slice(0,4) } },
+      department: { connect: { prefix: courseCode.slice(0, 4) } },
       description,
     });
+    setIsSubmitting(false);
+    setIsDialogOpen(false);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-2xl justify-center flex flex-col bg-zinc-600 text-black">
-      <input
-        type="text"
-        value={courseCode}
-        onChange={(e) => setCourseCode(e.target.value)}
-        placeholder="Course Code"
-        required
-      />
-      <input
-        type="text"
-        value={courseName}
-        onChange={(e) => setCourseName(e.target.value)}
-        placeholder="Course Name"
-        required
-      />
-      <textarea
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        placeholder="Description"
-      />
-      <button type="submit">Add Course</button>
-    </form>
+    <>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogTrigger asChild>
+          <Button
+            onClick={() => setIsDialogOpen(true)}
+            className="w-full"
+            variant={"outline"}
+          >
+            Add Course
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add Course</DialogTitle>
+          </DialogHeader>
+          <form onSubmit={handleSubmit} className="max-w-2xl space-y-3">
+            <div>
+              <Label htmlFor="courseCode">Course Code</Label>
+              <Input
+                id="courseCode"
+                type="text"
+                value={courseCode}
+                onChange={(e) => setCourseCode(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="courseName">Course Name</Label>
+              <Input
+                id="courseName"
+                type="text"
+                value={courseName}
+                onChange={(e) => setCourseName(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="description">Description</Label>
+              <Textarea
+                id="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="w-full flex justify-end gap-8 pt-6">
+              <DialogClose asChild>
+                <Button variant={"outline"} disabled={isSubmitting}>
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button type="submit" disabled={isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -1,15 +1,36 @@
 import { getAllDepartments } from "@/lib/dbQueries";
 import CourseEditor from "./CourseEditor";
-// import AddDepartment from "./AddDepartment";
-// import AddProfessor from "./AddProfessor";
+import { Button } from "@/components/ui/button";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import AddCourse from "./AddCourse";
+import AddProfessor from "./AddProfessor";
+import AddDepartment from "./AddDepartment";
 
 export default async function Page() {
-
   const departments = await getAllDepartments();
   return (
-    <div>
-      {/* <AddDepartment /> */}
-      {departments.map((department, i) => {
+    <div className="flex justify-center">
+      <Tabs defaultValue="creator" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="creator">Creator</TabsTrigger>
+          <TabsTrigger value="editor">Editor</TabsTrigger>
+        </TabsList>
+        
+        <TabsContent value="creator">
+          <AddCourse />
+          <AddProfessor departments={departments}/>
+          <AddDepartment/>
+
+        </TabsContent>
+        <TabsContent value="editor">
+          
+        </TabsContent>
+      </Tabs>
+
+      {/* {departments.map((department, i) => {
         return (
           <div key={i}>
             <h1 className="text-3xl p-2">{department.name}</h1>
@@ -18,8 +39,7 @@ export default async function Page() {
             </div>
           </div>
         );
-      })}
-      {/* <AddProfessor departments={departments} /> */}
+      })} */}
     </div>
   );
 }
