e64df7725af52cf2082cffb88922b354009a8f5e
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-03 23:36:07 +0530

    frontend navbar relocated

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import CourseList from '../components/CourseList';
-import CoursesHeader from '@/components/CoursesHeader';
+import React from "react";
+import CourseList from "../components/CourseList";
+import CoursesHeader from "@/components/CoursesHeader";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <div className='pt-10'>
+      <Navbar />
+      <div className="pt-10">
         <CoursesHeader />
-        <div className='container mx-auto px-4 py-8'>
+        <div className="container mx-auto px-4 py-8">
           <CourseList />
         </div>
       </div>
