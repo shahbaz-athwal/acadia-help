c85048360d6699e589c0eec11bcfe35b8f7a754b
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-07 23:18:05 +0530

    cleanup

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,16 +1,9 @@
 import React from "react";
-import CourseList from "../../components/CourseList";
-import CoursesHeader from "@/components/CoursesHeader";
 
 export default function Home() {
   return (
     <>
-      <div className="pt-10">
-        <CoursesHeader />
-        <div className="container mx-auto px-4 py-8">
-          <CourseList />
-        </div>
-      </div>
+      <div className="pt-10">Display All Departments</div>
     </>
   );
 }
