ac09e2d54cc02a0eba881d94aca22e6bca56c112
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-08 22:56:45 +0530

    notion private to public

--- apps/web/src/lib/notion-client-utils.ts
+++ apps/web/src/lib/notion-client-utils.ts
@@ -1,13 +1,7 @@
 import { NotionAPI } from "notion-client";
 
-const notion = new NotionAPI({
-  activeUser: process.env.NOTION_ACTIVE_USER,
-  authToken: process.env.NOTION_TOKEN_V2,
-});
+const notion = new NotionAPI();
 
 export async function getNotionPage(docId: string) {
-  console.log(docId);
   return await notion.getPage(docId);
 }
-
-
