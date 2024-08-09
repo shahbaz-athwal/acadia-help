40bde6b07fd70cfbe7e7b491f7063b845c0c4452
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-09 19:02:20 +0530

    rendering optimization

--- apps/web/src/components/CourseTabs.tsx
+++ apps/web/src/components/CourseTabs.tsx
@@ -1,9 +1,10 @@
+"use client";
+import { useState } from "react";
 import RenderNotion from "@/components/RenderNotion";
 import ReviewCard from "@/components/ReviewCard";
 import { ReviewChart } from "@/components/ReviewChart";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { getNotionPageMap } from "@/lib/notion-client-utils";
-// import { ExtendedRecordMap } from "notion-types";
+import { ExtendedRecordMap } from "notion-types";
 
 interface CourseTabsProps {
   ratingCount: number;
@@ -12,19 +13,34 @@ interface CourseTabsProps {
   docId: string;
 }
 
-const CourseTabs: React.FC<CourseTabsProps> = async ({
+const CourseTabs: React.FC<CourseTabsProps> = ({
   ratingCount,
   ratingDistribution,
   feedbacks,
   docId,
 }) => {
-  const pageData = await getNotionPageMap(docId);
+  const [loading, setLoading] = useState(true);
+  const [pageMap, setPageMap] = useState<ExtendedRecordMap | null>(null);
+
+  const handleDocTabSelect = async () => {
+    const response = await fetch(`/api/notionmap`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ docId }),
+    });
+
+    const { data } = await response.json();
+    setPageMap(data);
+    setLoading(false);
+  };
 
   return (
     <Tabs defaultValue="reviews" className="w-full pt-12">
       <TabsList className="grid w-full grid-cols-2">
         <TabsTrigger value="reviews">Reviews</TabsTrigger>
-        <TabsTrigger value="notion">
+        <TabsTrigger value="notion" onClick={handleDocTabSelect}>
           Doc
         </TabsTrigger>
       </TabsList>
@@ -45,12 +61,14 @@ const CourseTabs: React.FC<CourseTabsProps> = async ({
         </div>
       </TabsContent>
       <TabsContent value="notion">
-        {pageData ? (
-          <RenderNotion recordMap={pageData} />
+        {loading ? (
+          <div className="text-center py-10">Loading...</div>
+        ) : pageMap ? (
+          <RenderNotion recordMap={pageMap} />
         ) : (
           <div className="text-center pt-6 text-lg text-zinc-500">
             <div>We are working to get material for this course</div>
-            <div>Sorry for the inconvenience</div>
+            <div>Sorry for inconvinience</div>
           </div>
         )}
       </TabsContent>
