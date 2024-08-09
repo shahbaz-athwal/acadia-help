6cf715eff2114d883b8e1278cfcea8be4997e2cf
Shahbaz <shahbazathwal2107@gmail.com>
2024-08-09 19:02:06 +0530

    notionmap as route

---
+++ apps/web/src/app/api/notionmap/route.ts
@@ -0,0 +1,21 @@
+import { getNotionPageProperties } from "@/lib/notion";
+import { NotionAPI } from "notion-client";
+
+const notion = new NotionAPI();
+
+export async function POST(req: Request) {
+  const { docId } = await req.json();
+
+  const showPage = await getNotionPageProperties(docId);
+  if (showPage) {
+    const data = await notion.getPage(docId);
+    return Response.json({
+      success: true,
+      data,
+    });
+  }
+  return Response.json({
+    success: false,
+    data: null,
+  });
+}
